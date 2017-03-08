fd3ad9aa7f5f0e27cea1fc0510d258b086395cf7
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-03-08 14:45:48 -0500

    Use Kokkos::parallel_for when assigning the Morton codes.

--- packages/Kokkos/src/details/DTK_DetailsTreeConstruction.hpp
+++ packages/Kokkos/src/details/DTK_DetailsTreeConstruction.hpp
@@ -8,9 +8,32 @@ namespace DataTransferKit
 {
 namespace Details
 {
-// utilities for tree construction
+// Expands a 10-bit integer into 30 bits
+// by inserting 2 zeros after each bit.
 unsigned int expandBits( unsigned int v );
+
+// Calculates a 30-bit Morton code for the
+// given 3D point located within the unit cube [0,1].
 unsigned int morton3D( double x, double y, double z );
+
+namespace Functor
+{
+using Box = AABB;
+class AssignMortonCodes
+{
+  public:
+    AssignMortonCodes( Box const *bounding_boxes, unsigned int *morton_codes,
+                       Box const &scene_bounding_box );
+
+    void operator()( int const i ) const;
+
+  private:
+    Box const *_bounding_boxes;
+    unsigned int *_morton_codes;
+    Box const &_scene_bounding_box;
+};
+}
+// utilities for tree construction
 int countLeadingZeros( unsigned int k );
 int commonPrefix( unsigned int const *k, int n, int i, int j );
 int findSplit( unsigned int *sorted_morton_codes, int first, int last );
@@ -30,8 +53,20 @@ void calculateBoundingBoxOfTheScene( AABB const *bounding_boxes, int n,
     Kokkos::fence();
 }
 
+// to assign the Morton code for a given object, we use the centroid point of
+// its bounding box, and express it relative to the bounding box of the scene.
+template <typename ExecutionSpace>
 void assignMortonCodes( AABB const *bounding_boxes, unsigned int *morton_codes,
-                        int n, AABB const &scene_bounding_box );
+                        int n, AABB const &scene_bounding_box )
+{
+    Functor::AssignMortonCodes functor( bounding_boxes, morton_codes,
+                                        scene_bounding_box );
+    Kokkos::parallel_for( "assign_morton_codes",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, n ),
+                          functor );
+    Kokkos::fence();
+}
+
 void sortObjects( unsigned int *morton_codes, int *object_ids, int n );
 Node *generateHierarchy( unsigned int *sorted_morton_codes, int n,
                          Node *leaf_nodes, Node *internal_nodes );
