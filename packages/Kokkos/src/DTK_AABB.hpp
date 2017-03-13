b33cb9a135ecd153ad804df7164f68573b6edd76
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-03-13 15:19:13 -0400

    Replace Kokkos::Array by c array.

--- packages/Kokkos/src/DTK_AABB.hpp
+++ packages/Kokkos/src/DTK_AABB.hpp
@@ -2,7 +2,6 @@
 #define DTK_AABB_HPP
 
 #include <Kokkos_ArithTraits.hpp>
-#include <Kokkos_Array.hpp>
 #include <Kokkos_Core.hpp>
 
 namespace DataTransferKit
@@ -12,25 +11,30 @@ namespace DataTransferKit
 // a default constructor to initialize properly an "empty" box.
 struct AABB
 {
-    using ArrayType = Kokkos::Array<double, 6>;
-    using SizeType = ArrayType::size_type;
+    using ArrayType = double[6]; // Kokkos::Array<double, 6>;
+    using SizeType = size_t;     // ArrayType::size_type;
     AABB() = default;
     AABB( ArrayType const &minmax )
-        : _minmax( minmax )
     {
+        std::copy( minmax, minmax + 6, _minmax );
     }
     AABB &operator=( ArrayType const &minmax )
     {
-        _minmax = minmax;
+        std::copy( minmax, minmax + 6, _minmax );
         return *this;
     }
     double &operator[]( SizeType i ) { return _minmax[i]; }
     double const &operator[]( SizeType i ) const { return _minmax[i]; }
-    ArrayType _minmax = {{
+    volatile double &operator[]( SizeType i ) volatile { return _minmax[i]; }
+    volatile double const &operator[]( SizeType i ) volatile const
+    {
+        return _minmax[i];
+    }
+    ArrayType _minmax = {
         Kokkos::ArithTraits<double>::max(), -Kokkos::ArithTraits<double>::max(),
         Kokkos::ArithTraits<double>::max(), -Kokkos::ArithTraits<double>::max(),
         Kokkos::ArithTraits<double>::max(), -Kokkos::ArithTraits<double>::max(),
-    }};
+    };
     friend std::ostream &operator<<( std::ostream &os, AABB const &aabb )
     {
         os << "{";
