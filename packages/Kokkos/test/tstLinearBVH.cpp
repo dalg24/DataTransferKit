257d64109397a4d21f65252723dde338b5edaf6d
Bruno Turcksin <bruno.turcksin@gmail.com>
2017-03-24 10:06:30 -0400

    Use Kokkos view to test tree traversal.

--- packages/Kokkos/test/tstLinearBVH.cpp
+++ packages/Kokkos/test/tstLinearBVH.cpp
@@ -72,26 +72,42 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, structured_grid, NO )
             }
 
     using DeviceType = typename DataTransferKit::BVH<NO>::DeviceType;
+    using ExecutionSpace = typename DeviceType::execution_space;
     Kokkos::View<DataTransferKit::AABB *, DeviceType, Kokkos::MemoryUnmanaged>
         bounding_boxes( bounding_boxes_vector.data(), n );
     DataTransferKit::BVH<NO> bvh( bounding_boxes );
 
     // (i) use same objects for the queries than the objects we constructed the
     // BVH
-    for ( int i = 0; i < n; ++i )
+    Kokkos::View<int * [2], DeviceType> identity( "identity", n );
+
+    auto check_identity = KOKKOS_LAMBDA( const int i )
     {
         Overlap overlap_predicate( bounding_boxes[i] );
         auto collision = details::spatial_query( bvh, overlap_predicate );
-        // we expect the collision list to be diag(0, 1, ..., nx*ny*nz-1)
-        TEST_EQUALITY( collision.size(), 1 );
-        TEST_EQUALITY( *collision.begin(), i );
+        identity( i, 0 ) = collision.size();
+        identity( i, 1 ) = *collision.begin();
+    };
+
+    Kokkos::parallel_for( "check_identity",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, n ),
+                          check_identity );
+
+    // we expect the collision list to be diag(0, 1, ..., nx*ny*nz-1)
+    for ( int i = 0; i < n; ++i )
+    {
+        TEST_EQUALITY( identity( i, 0 ), 1 );
+        TEST_EQUALITY( identity( i, 1 ), i );
     }
 
     // (ii) use bounding boxes that overlap with first neighbors
+    // Compute the reference solution.
+    std::vector<std::set<int>> ref( n );
     for ( int i = 0; i < nx; ++i )
         for ( int j = 0; j < ny; ++j )
             for ( int k = 0; k < nz; ++k )
             {
+                int const index = ind( i, j, k );
                 // bounding box around nodes of the structured grid will overlap
                 // with neighboring nodes
                 bounding_boxes[ind( i, j, k )] = {
@@ -101,73 +117,97 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, structured_grid, NO )
                 };
                 // fill in reference solution to check againt the collision list
                 // computed during the tree traversal
-                std::set<int> ref;
                 if ( ( i > 0 ) && ( j > 0 ) && ( k > 0 ) )
-                    ref.emplace( ind( i - 1, j - 1, k - 1 ) );
+                    ref[index].emplace( ind( i - 1, j - 1, k - 1 ) );
                 if ( ( i > 0 ) && ( k > 0 ) )
-                    ref.emplace( ind( i - 1, j, k - 1 ) );
+                    ref[index].emplace( ind( i - 1, j, k - 1 ) );
                 if ( ( i > 0 ) && ( j < ny - 1 ) && ( k > 0 ) )
-                    ref.emplace( ind( i - 1, j + 1, k - 1 ) );
+                    ref[index].emplace( ind( i - 1, j + 1, k - 1 ) );
                 if ( ( i > 0 ) && ( j > 0 ) )
-                    ref.emplace( ind( i - 1, j - 1, k ) );
+                    ref[index].emplace( ind( i - 1, j - 1, k ) );
                 if ( i > 0 )
-                    ref.emplace( ind( i - 1, j, k ) );
+                    ref[index].emplace( ind( i - 1, j, k ) );
                 if ( ( i > 0 ) && ( j < ny - 1 ) )
-                    ref.emplace( ind( i - 1, j + 1, k ) );
+                    ref[index].emplace( ind( i - 1, j + 1, k ) );
                 if ( ( i > 0 ) && ( j > 0 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i - 1, j - 1, k + 1 ) );
+                    ref[index].emplace( ind( i - 1, j - 1, k + 1 ) );
                 if ( ( i > 0 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i - 1, j, k + 1 ) );
+                    ref[index].emplace( ind( i - 1, j, k + 1 ) );
                 if ( ( i > 0 ) && ( j < ny - 1 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i - 1, j + 1, k + 1 ) );
+                    ref[index].emplace( ind( i - 1, j + 1, k + 1 ) );
 
                 if ( ( j > 0 ) && ( k > 0 ) )
-                    ref.emplace( ind( i, j - 1, k - 1 ) );
+                    ref[index].emplace( ind( i, j - 1, k - 1 ) );
                 if ( k > 0 )
-                    ref.emplace( ind( i, j, k - 1 ) );
+                    ref[index].emplace( ind( i, j, k - 1 ) );
                 if ( ( j < ny - 1 ) && ( k > 0 ) )
-                    ref.emplace( ind( i, j + 1, k - 1 ) );
+                    ref[index].emplace( ind( i, j + 1, k - 1 ) );
                 if ( j > 0 )
-                    ref.emplace( ind( i, j - 1, k ) );
+                    ref[index].emplace( ind( i, j - 1, k ) );
                 if ( true )
-                    ref.emplace( ind( i, j, k ) );
+                    ref[index].emplace( ind( i, j, k ) );
                 if ( j < ny - 1 )
-                    ref.emplace( ind( i, j + 1, k ) );
+                    ref[index].emplace( ind( i, j + 1, k ) );
                 if ( ( j > 0 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i, j - 1, k + 1 ) );
+                    ref[index].emplace( ind( i, j - 1, k + 1 ) );
                 if ( k < nz - 1 )
-                    ref.emplace( ind( i, j, k + 1 ) );
+                    ref[index].emplace( ind( i, j, k + 1 ) );
                 if ( ( j < ny - 1 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i, j + 1, k + 1 ) );
+                    ref[index].emplace( ind( i, j + 1, k + 1 ) );
 
                 if ( ( i < nx - 1 ) && ( j > 0 ) && ( k > 0 ) )
-                    ref.emplace( ind( i + 1, j - 1, k - 1 ) );
+                    ref[index].emplace( ind( i + 1, j - 1, k - 1 ) );
                 if ( ( i < nx - 1 ) && ( k > 0 ) )
-                    ref.emplace( ind( i + 1, j, k - 1 ) );
+                    ref[index].emplace( ind( i + 1, j, k - 1 ) );
                 if ( ( i < nx - 1 ) && ( j < ny - 1 ) && ( k > 0 ) )
-                    ref.emplace( ind( i + 1, j + 1, k - 1 ) );
+                    ref[index].emplace( ind( i + 1, j + 1, k - 1 ) );
                 if ( ( i < nx - 1 ) && ( j > 0 ) )
-                    ref.emplace( ind( i + 1, j - 1, k ) );
+                    ref[index].emplace( ind( i + 1, j - 1, k ) );
                 if ( i < nx - 1 )
-                    ref.emplace( ind( i + 1, j, k ) );
+                    ref[index].emplace( ind( i + 1, j, k ) );
                 if ( ( i < nx - 1 ) && ( j < ny - 1 ) )
-                    ref.emplace( ind( i + 1, j + 1, k ) );
+                    ref[index].emplace( ind( i + 1, j + 1, k ) );
                 if ( ( i < nx - 1 ) && ( j > 0 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i + 1, j - 1, k + 1 ) );
+                    ref[index].emplace( ind( i + 1, j - 1, k + 1 ) );
                 if ( ( i < nx - 1 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i + 1, j, k + 1 ) );
+                    ref[index].emplace( ind( i + 1, j, k + 1 ) );
                 if ( ( i < nx - 1 ) && ( j < ny - 1 ) && ( k < nz - 1 ) )
-                    ref.emplace( ind( i + 1, j + 1, k + 1 ) );
+                    ref[index].emplace( ind( i + 1, j + 1, k + 1 ) );
+            }
 
-                // traverse the tree and find potential collisions
-                Overlap overlap_predicate( bounding_boxes[ind( i, j, k )] );
+    Kokkos::View<int * [2], DeviceType> first_neighbor( "first_neighbor", n );
+
+    auto check_first_neighbor = KOKKOS_LAMBDA( const int i )
+    {
+        for ( int j = 0; j < ny; ++j )
+            for ( int k = 0; k < nz; ++k )
+            {
+                int const index = ind( i, j, k );
+                Overlap overlap_predicate( bounding_boxes[index] );
                 auto collision =
                     details::spatial_query( bvh, overlap_predicate );
-                // check the answer is the same as the reference we computed
-                TEST_EQUALITY( collision.size(), ref.size() );
-                for ( auto const &x : collision )
-                    TEST_EQUALITY( ref.count( x ), 1 );
+                first_neighbor( index, 0 ) = collision.size();
+                // Only check the first element because we don't know how many
+                // elements there are when we build the View
+                first_neighbor( index, 1 ) = *collision.begin();
             }
+    };
+
+    Kokkos::parallel_for( "check_first_neighbor",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, nx ),
+                          check_first_neighbor );
+
+    for ( int i = 0; i < nx; ++i )
+        for ( int j = 0; j < ny; ++j )
+            for ( int k = 0; k < nz; ++k )
+            {
+                int index = ind( i, j, k );
+                TEST_EQUALITY( first_neighbor( index, 0 ),
+                               static_cast<int>( ref[index].size() ) );
+                TEST_EQUALITY( ref[index].count( first_neighbor( index, 1 ) ),
+                               1 );
+            }
+
     // (iii) use random points
     std::default_random_engine generator;
     std::uniform_real_distribution<double> distribution_x( 0.0, Lz );
@@ -176,26 +216,24 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, structured_grid, NO )
 
     int nn = 1000;
     int count = 0; // drop point if mapped into [0.5-eps], 0.5+eps]^3
+    Kokkos::View<DataTransferKit::AABB *, ExecutionSpace> aabb( "aabb", nn );
+    std::vector<int> indices( nn );
     for ( int l = 0; l < nn; ++l )
     {
         double x = distribution_x( generator );
         double y = distribution_y( generator );
         double z = distribution_z( generator );
-        DataTransferKit::AABB aabb;
-        aabb = {
+        aabb[l] = {
             x - 0.5 * Lx / ( nx - 1 ), x + 0.5 * Lx / ( nx - 1 ),
             y - 0.5 * Ly / ( ny - 1 ), y + 0.5 * Ly / ( ny - 1 ),
             z - 0.5 * Lz / ( nz - 1 ), z + 0.5 * Lz / ( nz - 1 ),
         };
 
-        Overlap overlap_predicate( aabb );
-        auto collision = details::spatial_query( bvh, overlap_predicate );
         int i = std::round( x / Lx * ( nx - 1 ) );
         int j = std::round( y / Ly * ( ny - 1 ) );
         int k = std::round( z / Lz * ( nz - 1 ) );
         // drop point if it the bounding box is going to overlap with more than
-        // one
-        // bounding box
+        // one bounding box
         if ( ( std::abs( x / Lx * ( nx - 1 ) -
                          std::floor( x / Lx * ( nx - 1 ) ) - 0.5 ) < eps ) ||
              ( std::abs( y / Ly * ( ny - 1 ) -
@@ -206,9 +244,30 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, structured_grid, NO )
             ++count;
             continue;
         }
-        TEST_EQUALITY( collision.size(), 1 );
-        TEST_EQUALITY( *collision.begin(), ind( i, j, k ) );
+        // Save the indices for the check
+        indices[l] = ind( i, j, k );
     }
+
+    Kokkos::View<int * [2], DeviceType> random( "random", n );
+    auto check_random = KOKKOS_LAMBDA( const int i )
+    {
+
+        Overlap overlap_predicate( aabb[i] );
+        auto collision = details::spatial_query( bvh, overlap_predicate );
+        random( i, 0 ) = collision.size();
+        random( i, 1 ) = *collision.begin();
+    };
+
+    Kokkos::parallel_for( "check_random",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, nn ),
+                          check_random );
+
+    for ( int i = 0; i < nn; ++i )
+    {
+        TEST_EQUALITY( random( i, 0 ), 1 );
+        TEST_EQUALITY( random( i, 1 ), indices[i] );
+    }
+
     // make sure we did not drop all points
     TEST_COMPARE( count, <, n );
 }
@@ -254,8 +313,8 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, rtree, NO )
 {
     namespace bg = boost::geometry;
     namespace bgi = boost::geometry::index;
-    using Point = bg::model::point<double, 3, bg::cs::cartesian>;
-    using RTree = bgi::rtree<std::pair<Point, int>, bgi::linear<16>>;
+    using BPoint = bg::model::point<double, 3, bg::cs::cartesian>;
+    using RTree = bgi::rtree<std::pair<BPoint, int>, bgi::linear<16>>;
 
     // contruct a cloud of points (nodes of a structured grid)
     double Lx = 10.0;
@@ -275,7 +334,7 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, rtree, NO )
         double x = std::get<0>( point );
         double y = std::get<1>( point );
         double z = std::get<2>( point );
-        rtree.insert( std::make_pair( Point( x, y, z ), i ) );
+        rtree.insert( std::make_pair( BPoint( x, y, z ), i ) );
     }
 
     // build bounding volume hierarchy
@@ -297,7 +356,21 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, rtree, NO )
 
     // random points for radius search and kNN queries
     // compare our solution against Boost R-tree
-    auto queries = make_random_cloud( Lx, Ly, Lz, 100 );
+    int const n_points = 100;
+    auto queries = make_random_cloud( Lx, Ly, Lz, n_points );
+    using ExecutionSpace = typename DeviceType::execution_space;
+    Kokkos::View<double * [3], ExecutionSpace> point_coords( "point_coords",
+                                                             n_points );
+    Kokkos::View<double *, ExecutionSpace> radii( "radii", n_points );
+    Kokkos::View<int * [2], ExecutionSpace> within_n_pts( "within_n_pts",
+                                                          n_points );
+    Kokkos::View<int * [2], ExecutionSpace> nearest_n_pts( "nearest_n_pts",
+                                                           n_points );
+    Kokkos::View<int *, ExecutionSpace> k( "distribution_k", n_points );
+    std::vector<std::vector<std::pair<BPoint, int>>> returned_values_within(
+        n_points );
+    std::vector<std::vector<std::pair<BPoint, int>>> returned_values_nearest(
+        n_points );
     // use random radius for the search and random number k of for the kNN
     // search
     std::default_random_engine generator;
@@ -305,73 +378,87 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( LinearBVH, rtree, NO )
         0.0, std::sqrt( Lx * Lx + Ly * Ly + Lz * Lz ) );
     std::uniform_int_distribution<int> distribution_k(
         1, std::floor( sqrt( nx * nx + ny * ny + nz * nz ) ) );
-    for ( auto const &point : queries )
+    for ( unsigned int i = 0; i < n_points; ++i )
     {
+        auto const &point = queries[i];
         double x = std::get<0>( point );
         double y = std::get<1>( point );
         double z = std::get<2>( point );
-        Point centroid( x, y, z );
-        double radius = distribution_radius( generator );
-        int k = distribution_k( generator );
+        BPoint centroid( x, y, z );
+        radii[i] = distribution_radius( generator );
+        k[i] = distribution_k( generator );
+        double radius = radii[i];
 
         // COMMENT: Did not implement proper radius search yet
         // This use available tree traversal for axis-aligned bounding box and
         // filters out candidates afterwards.
         // The coordinates of the points in the structured cloud (source) are
         // accessed directly and we use Boost to compute the distance.
-        // This will need to be cleaned up, possibly with a templated tree
-        // traversal with a predicate.
-        details::Within within_predicate( {x, y, z}, radius );
-        auto sol_within = details::spatial_query( bvh, within_predicate );
+        point_coords( i, 0 ) = x;
+        point_coords( i, 1 ) = y;
+        point_coords( i, 2 ) = z;
 
         // use the R-tree to obtain a reference solution
-        std::vector<std::pair<Point, int>> returned_values;
         rtree.query( bgi::satisfies( [centroid, radius](
-                         std::pair<Point, int> const &val ) {
+                         std::pair<BPoint, int> const &val ) {
                          return bg::distance( centroid, val.first ) <= radius;
                      } ),
-                     std::back_inserter( returned_values ) );
-
-        // I tried to encapsulate this in a lambda but it won't work with the
-        // Teuchos test assertion macros
-        // Anyway, this is fine for now...
-        {
-            auto const &ref = returned_values;
-            TEST_EQUALITY( sol_within.size(), ref.size() );
-
-            std::set<int> ref_ids;
-            for ( auto const &id : ref )
-                ref_ids.emplace( id.second );
-
-            for ( auto const &id : sol_within )
-                TEST_EQUALITY( ref_ids.count( id ), 1 );
-        }
+                     std::back_inserter( returned_values_within[i] ) );
 
         // k nearest neighbors
-        returned_values.clear();
+        rtree.query( bgi::nearest( BPoint( x, y, z ), k[i] ),
+                     std::back_inserter( returned_values_nearest[i] ) );
+    }
 
-        rtree.query( bgi::nearest( Point( x, y, z ), k ),
-                     std::back_inserter( returned_values ) );
-        std::list<std::pair<int, double>> sol;
-        sol = DataTransferKit::Details::nearest_query( bvh, {x, y, z}, k );
-        // copy/paste of the check solution against reference above
-        {
-            auto const &ref = returned_values;
-            auto compare = []( std::pair<int, double> const &a,
-                               std::pair<int, double> const &b ) {
-                return a.first < b.first;
-            };
-            std::set<std::pair<int, double>, decltype( compare )> tmp(
-                compare );
-            tmp.insert( sol.begin(), sol.end() );
-            TEST_EQUALITY( sol.size(), ref.size() );
-            for ( auto const &x : ref )
-            {
-                auto it = tmp.find( std::make_pair( x.second, -1.0 ) );
-                TEST_ASSERT( it != tmp.end() );
-                TEST_EQUALITY( it->second, bg::distance( centroid, x.first ) );
-            }
-        }
+    auto random_within_lambda = KOKKOS_LAMBDA( const int i )
+    {
+        details::Within within_predicate(
+            {point_coords( i, 0 ), point_coords( i, 1 ), point_coords( i, 2 )},
+            radii( i ) );
+        auto sol_within = details::spatial_query( bvh, within_predicate );
+        within_n_pts( i, 0 ) = sol_within.size();
+        within_n_pts( i, 1 ) = *sol_within.begin();
+    };
+
+    Kokkos::parallel_for( "random_within",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, n_points ),
+                          random_within_lambda );
+
+    for ( int i = 0; i < n_points; ++i )
+    {
+        auto const &ref = returned_values_within[i];
+        TEST_EQUALITY( within_n_pts( i, 0 ), static_cast<int>( ref.size() ) );
+        std::set<int> ref_ids;
+        for ( auto const &id : ref )
+            ref_ids.emplace( id.second );
+
+        if ( ref.size() > 0 )
+            TEST_EQUALITY( ref_ids.count( within_n_pts( i, 1 ) ), 1 );
+    }
+
+    auto random_nearest_lambda = KOKKOS_LAMBDA( const int i )
+    {
+        auto sol_nearest = DataTransferKit::Details::nearest_query(
+            bvh,
+            {point_coords( i, 0 ), point_coords( i, 1 ), point_coords( i, 2 )},
+            k[i] );
+        nearest_n_pts( i, 0 ) = sol_nearest.size();
+        nearest_n_pts( i, 1 ) = sol_nearest.begin()->first;
+    };
+
+    Kokkos::parallel_for( "random_nearest",
+                          Kokkos::RangePolicy<ExecutionSpace>( 0, n_points ),
+                          random_nearest_lambda );
+
+    for ( int i = 0; i < n_points; ++i )
+    {
+        auto const &ref = returned_values_nearest[i];
+        TEST_EQUALITY( nearest_n_pts( i, 0 ), static_cast<int>( ref.size() ) );
+        std::set<int> ref_ids;
+        for ( auto const &id : ref )
+            ref_ids.emplace( id.second );
+
+        TEST_EQUALITY( ref_ids.count( nearest_n_pts( i, 1 ) ), 1 );
     }
 }
 
