a37199ac8920ee31ddcf463ccc93ca8f040252ed
Damien L-G <dalg24@gmail.com>
2018-05-15 10:58:50 +0000

    [do not merge] make all pacakges but Utils and Search optional

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,9 +1,9 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
     Utils                 packages/Utils                      ST  REQUIRED
-    Interface             packages/Interface                  ST  REQUIRED
+    Interface             packages/Interface                  ST  OPTIONAL
     Search                packages/Search                     ST  REQUIRED
-    Discretization        packages/Discretization             ST  REQUIRED
-    Meshfree              packages/Meshfree                   ST  REQUIRED
+    Discretization        packages/Discretization             ST  OPTIONAL
+    Meshfree              packages/Meshfree                   ST  OPTIONAL
     HybridTransport       packages/Benchmarks/HybridTransport ST  OPTIONAL
   )
